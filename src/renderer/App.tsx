4efe0b85b3dda4c0dc35632a1f7f74e147846110
Ankur Mehta <51302878+ankurmehta102@users.noreply.github.com>
2023-07-15 02:21:44 +0530

    initial commit with tailwind interation

---
+++ src/renderer/App.tsx
@@ -0,0 +1,20 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function Hello() {
+  return (
+    <>
+      <div className="bg-gray-500">Hello</div>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
