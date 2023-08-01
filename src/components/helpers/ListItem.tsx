9eac0741c125378d721150475db3760cef32a723
Ankur Mehta <51302878+ankurmehta102@users.noreply.github.com>
2023-08-01 10:33:38 +0530

    [feature] add active link state in sidebar

--- src/components/helpers/ListItem.tsx
+++ src/components/helpers/ListItem.tsx
@@ -1,5 +1,4 @@
-import { ReactNode } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 type ListItemPropTypes = {
   children: any;
@@ -9,21 +8,29 @@ type ListItemPropTypes = {
 };
 
 const ListItem = ({ children, isLinks, onClick, to }: ListItemPropTypes) => {
+  const location = useLocation();
   return (
     <li
       onClick={onClick}
       className={`${
         isLinks
-          ? 'my-4 h-full w-full rounded p-1 hover:bg-fuchsia-200 hover:text-black'
-          : `  h-full w-full ${
+          ? `my-4 h-full w-full rounded   hover:bg-fuchsia-200 hover:text-black ${
+              location.pathname === to
+                ? 'bg-fuchsia-200 text-black'
+                : 'text-custom-slate'
+            }`
+          : `  h-full w-full text-custom-slate ${
               children?.type?.name === 'AiOutlineClose'
                 ? 'hover:bg-red-500 hover:text-black'
                 : 'hover:bg-sidebar'
             } `
-      } flex items-center justify-center text-custom-slate duration-150 ease-in`}
+      } flex items-center justify-center duration-150 ease-in`}
     >
       {isLinks ? (
-        <Link to={to || ''} className="text-2xl">
+        <Link
+          to={to || ''}
+          className={` flex h-full w-full items-center justify-center p-1 text-2xl`}
+        >
           {children}
         </Link>
       ) : (
